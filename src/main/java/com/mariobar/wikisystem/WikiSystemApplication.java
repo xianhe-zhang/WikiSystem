a89144a21c63161923ce04266db4c184edfccdd1
xianhezhang <3600276843@qq.com>
2022-01-18 00:10:26 -0800

    first initial

---
+++ src/main/java/com/mariobar/wikisystem/WikiSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mariobar.wikisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariobar/wikisystem/WikiSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariobar.wikisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

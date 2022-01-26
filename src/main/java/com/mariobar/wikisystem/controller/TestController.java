d1fdb035bd93a95c4bbccaadb2f6d9ec22e65c68
xianhezhang <3600276843@qq.com>
2022-01-25 22:36:51 -0800

    3-7 电子书表格开发

--- src/main/java/com/mariobar/wikisystem/controller/TestController.java
+++ src/main/java/com/mariobar/wikisystem/controller/TestController.java
@@ -1,15 +1,13 @@
 package com.mariobar.wikisystem.controller;
 
-import com.mariobar.wikisystem.domain.Test;
-import com.mariobar.wikisystem.service.TestService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+//import com.mariobar.wikisystem.service.TestService;
 
 @RestController
 public class TestController {
@@ -18,9 +16,9 @@ public class TestController {
 
   @Value("${test.hello:TEST}")
   private String testHello;
-
-  @Resource
-  private TestService testService;
+//
+//  @Resource
+//  private TestService testService;
 
 //  @Resource
 //  private RedisTemplate redisTemplate;
@@ -46,11 +44,11 @@ public class TestController {
   public String helloPost(String name) {
     return "Hello World! Post，" + name;
   }
-
-  @GetMapping("/test/list")
-  public List<Test> list() {
-    return testService.list();
-  }
+//
+//  @GetMapping("/test/list")
+//  public List<Test> list() {
+//    return testService.list();
+//  }
 
 //  @RequestMapping("/redis/set/{key}/{value}")
 //  public String set(@PathVariable Long key, @PathVariable String value) {
